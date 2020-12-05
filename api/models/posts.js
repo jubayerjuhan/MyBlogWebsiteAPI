730e0ffd57a29a5401e311bf69bdf6557ec221ff
jubayerjuhan <davidjuhan23@gmail.com>
2020-12-05 20:34:30 +0600

    Get to know how to get data from post ID

--- api/models/posts.js
+++ api/models/posts.js
@@ -1,26 +1,36 @@
 const PATH = "./data.json";
 const fs = require('fs');
-class Post{
+class Post {
 
     get() {
         //!get List Of Blog Post
         return this.readData();
     }
 
-    getIndividualBlog() {
+    getIndividualBlog(postID) {
         //!Get Individual Blog Post
-
+        const posts = this.readData();
+        const foundPost = posts.find((post) => post.id == postID);
+        return foundPost;
     }
 
-    add() {
+    add(newPost) {
         //!Add New Post
+        const currentPost = this.readData();
+        currentPost.unshift(newPost);
+        this.storeData(currentPost);
 
     }
 
-    readData (){
+    readData() {
         let rawdata = fs.readFileSync(PATH);
         let posts = JSON.parse(rawdata);
         return posts;
+    }
+
+    storeData(rawData) {
+        let data = JSON.stringify(rawData);
+        fs.writeFileSync(PATH, data);
     }
 
 }
--- app.js
+++ app.js
@@ -4,10 +4,21 @@ const Post = require('./api/models/posts');
 const postsData = new Post();
 
 
-app.get("/api/posts",(req,res) =>{
+app.get("/api/posts", (req, res) => {
     res.status(200).send(postsData.get());
 })
 
-app.listen(3000, () =>{
+app.get("/api/posts/:post_id", (req, res) => {
+    const postID = req.params.post_id;
+    const foundPost = postsData.getIndividualBlog(postID);
+    if (foundPost) {
+        res.status(200).send(foundPost);
+    } else {
+        res.status(404).send("Not Found");
+    }
+})
+
+
+app.listen(3000, () => {
     console.log("Listening On http://localhost:3000")
 })
